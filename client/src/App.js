0022f4ae0eb494308522fe07a00e83573e14b24d
courtney23 <courtcross98@gmail.com>
2020-04-01 15:46:03 -0400

    added files

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,22 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
+import Form from "./views/Form/Form"
+import Listings from "./views/Listings/Listings"
+import Messages from "./views/Messages/Messages"
 
 
 const App = () => {
   return (
     <div>
-      <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
+        <Route exact path="/Form" component={Form} />
+        <Route exact path="/Listings" component={Listings} />
+        <Route exact path="/Messages" component={Messages} />
         <Route component={NotFound}/>
       </Switch>
     </div>
---
+++ client/src/views/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Form.css';
+
+function Form() {
+    return (
+        <div className="App">
+            <p>put component here</p>
+        </div>
+    );
+}
+
+export default Form;
