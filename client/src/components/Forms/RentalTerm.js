fdcf47ed3d551130cd4feb1e9d844da94baa6445
samanthasu <sus@ufl.edu>
2020-04-14 16:09:29 -0400

    form styling

--- client/src/components/Forms/RentalTerm.js
+++ client/src/components/Forms/RentalTerm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Forms.css';
+import { Button, Form, Col, InputGroup } from 'react-bootstrap';
 
 class RentalTerm extends React.Component {
     constructor(props) {
@@ -35,14 +36,14 @@ class RentalTerm extends React.Component {
         }
 
         var today = new Date();
-        var month = (today.getMonth() + 1); 
-        if (month<10) month = '0' + month;
+        var month = (today.getMonth() + 1);
+        if (month < 10) month = '0' + month;
         var day = today.getDate();
-        if(day < 10) day = '0' + day;
+        if (day < 10) day = '0' + day;
 
         var date = today.getFullYear() + '-' + month + '-' + day;
 
-        if(new Date(date) > new Date(start)){
+        if (new Date(date) > new Date(start)) {
             return false;
         }
 
@@ -85,27 +86,35 @@ class RentalTerm extends React.Component {
         } = this.state;
         return (
             <div>
-                <h2>Choose a Rental Term</h2>
-                <hr></hr>
-                <input
-                    type="date"
-                    placeholder="MM/DD/YYYY"
-                    value={startDate}
-                    onChange={this.onTextboxChangeStartDate}
-                ></input><br />
-                <input
-                    type="date"
-                    placeholder="MM/DD/YYYY"
-                    value={endDate}
-                    onChange={this.onTextboxChangeEndDate}
-                ></input><br />
+                <Form>
+                    <div class="forms">
+                        <h2>Choose a Rental Term</h2>
+                        <hr></hr>
+                        <input
+                            type="date"
+                            placeholder="MM/DD/YYYY"
+                            value={startDate}
+                            onChange={this.onTextboxChangeStartDate}
+                        ></input><br />
+                        <input
+                            type="date"
+                            placeholder="MM/DD/YYYY"
+                            value={endDate}
+                            onChange={this.onTextboxChangeEndDate}
+                        ></input><br />
 
-                <button onClick={this.props.back}> Back </button>
-                <button onClick={this.nextPage}> Next </button>
-                <div>
-                    {error}
-                </div>
-            </div>
+                        <div class="buttons">
+                            <Button variant="outline-info" size="sm" onClick={this.props.back}> Back </Button>
+                            <button onClick={this.nextPage}> Next </button>
+                        </div>
+                    </div>
+
+                    <div>
+                        {error}
+                    </div>
+                </Form >
+
+            </div >
         )
     };
 }
