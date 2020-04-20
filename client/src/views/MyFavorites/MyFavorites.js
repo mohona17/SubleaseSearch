fec5dc1733cfca0cb6ee960327abaab06f56a8bc
mohona17 <mohonaahmed@ufl.edu>
2020-04-19 18:42:42 -0700

    adding my favs and profile page

---
+++ client/src/views/MyFavorites/MyFavorites.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './MyFavorites.css';
+
+class MyFavorites extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <h1>My Favorites</h1>
+        )
+    };
+}
+
+export default MyFavorites;
---
+++ client/src/views/Profile/Profile.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Profile.css';
+import MyListings from '../Listings/MyListings'
+import Messages from '../Messages/Messages'
+// import MyFavorites from '../MyFavorites/MyFavorites';
+
+
+
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>My Profile</h1>
+                <div class="row justify-content-md-center">
+                    <div class="col col-sm-2 text-center">
+                        <img class="image" src="https://images.squarespace-cdn.com/content/v1/54bbd50ce4b05e8a36418abc/1533226867020-NALD4HA8GBL3IUIQE9PM/ke17ZwdGBToddI8pDm48kMh3mVmBaCAeGwqCLG3iONRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PITeQtWPcxF65ANawkK25DREOmFck9peR6QL8AnpRiPJE/rachel-rouhana-profile-picture-circle.png"></img>
+                    </div>
+                    <div class="col col-sm-2 text-center">
+                        <h3>Jane Doe</h3>
+                        <h5>jdoe@ufl.edu</h5>
+                        <h5>(555)-555-5555</h5>
+                    </div>
+                    <div class="col col-sm-6 text-center">
+                        <MyListings></MyListings>
+                    </div>
+                </div>
+                <div class="row justify-content-md-center">
+                    <div class="col col-sm-4 text-center">
+                        {/* <MyFavorites></MyFavorites> */}
+                    </div>
+                    <div class="col col-sm-4 text-center">
+                        <Messages></Messages>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+}
+
+export default Profile;
