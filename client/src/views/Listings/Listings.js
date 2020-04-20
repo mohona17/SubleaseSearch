daf6a635a4827f7a7e0392b33ba68a8ccd063361
Deanna Gran <32341520+deannagran@users.noreply.github.com>
2020-04-20 01:23:07 -0400

    kinda sucks still but improved

--- client/src/views/Listings/Listings.js
+++ client/src/views/Listings/Listings.js
@@ -10,7 +10,6 @@ import PropTypes from 'prop-types';
 import Header from '../../components/Header/Header'
 
 
-
 let urls = [
     "./assets/1.jpg",
     "./assets/2.jpg",
@@ -23,31 +22,29 @@ let urls = [
       "complex": "College Park",
       "bedrooms": "2",
       "bathrooms": "1",
-      "pets": "No"
+      "pets": "No",
+      "image": "https://medialibrarycdn.entrata.com/media_library/10357/5c954fe84b22b0.27295052437.png"
     },
     {
         "complex": "The Standard",
         "bedrooms": "4",
         "bathrooms": "2",
-        "pets": "Yes"
-      },
-      {
-        "complex": "N/A",
-        "bedrooms": "3",
-        "bathrooms": "2",
-        "pets": "No"
+        "pets": "Yes",
+        "image": "https://img.swamprentals.com/uploads/images/College%20Park%20at%20Midtown%20Apartments%20Photos24.jpeg"
       },
       {
         "complex": "The Courtyards",
         "bedrooms": "4",
         "bathrooms": "3.5",
-        "pets": "Yes"
+        "pets": "Yes",
+        "image": "https://img.swamprentals.com/uploads/images/CY10.jpg"
       },
       {
         "complex": "College Manor",
         "bedrooms": "2",
         "bathrooms": "2",
-        "pets": "No"
+        "pets": "No",
+        "image": "https://www.rentgainesvilleapartments.com/wp-content/uploads/2013/12/Courtyard-Apartments-Dining-Room-08.jpg"
       }
       
   ];
@@ -56,13 +53,14 @@ let urls = [
     rowData: plugins.LocalPlugin.selectors.rowDataSelector(state, props)
   }))(({ rowData }) => (
     <div style={{
-      backgroundColor: "#EDEDED",
+      backgroundColor: "#DDF5E7",
       border: "1px solid #777",
       padding: 5,
       margin: 10,
     }}>
       <h1>{rowData.name}</h1>
       <ul>
+      <img src={rowData.image} alt="Image" height="200" width="200"></img>
       <li><strong>Complex</strong>: {rowData.complex}</li>
       <li><strong>Bedrooms</strong>: {rowData.bedrooms}</li>
       <li><strong>Bathrooms</strong>: {rowData.bathrooms}</li>
@@ -87,6 +85,15 @@ let urls = [
     </div>
   );
 
+  const griddleLayout = ({Table, Pagination, Filter, SettingsWrapper}) => (
+    <div>
+        <Filter/>
+        <Pagination/>
+        <Table/>
+
+    </div>
+  );
+
 class Listings extends React.Component {
     constructor(props) {
         super(props);
@@ -105,9 +112,10 @@ class Listings extends React.Component {
                 }}
                 plugins={[plugins.LocalPlugin]}
                 components={{
+                Layout: griddleLayout,
                 Row: CustomRowComponent,
                 TableContainer: CustomTableComponent,
-                TableBody: CustomTableBody,
+                TableBody: CustomTableBody
                 }}/>      
         </div>
 
